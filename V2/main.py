d85e0e58ec4e36448c58bbeae6b0856792ebc545
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-10-08 23:59:50 +0400

    PROGRESS: new thoughts / architecture on how to handle email audits

--- V2/main.py
+++ V2/main.py
@@ -7,13 +7,12 @@ from pathlib import Path
 import shutil
 
 
-# main.py for V2 will be importing the calculations from audits.py
-# then, running the calculations in a multithreaded manner on all existing CSV files in the CSVs folder
-# using multithreading to manage multiple threads in auditing multiple tables at the same time, and manipulating the same
-# output string in a orderely, ticketed manner.
+"""
+V2 Notes:
 
-# after the script is completed running, the results are outputted, and the CSVs in the CSVs folder are cleaned up.
+I must do the two audits separately, and construct them separately.
 
+"""
 
 
 # define the target folder for processing:
